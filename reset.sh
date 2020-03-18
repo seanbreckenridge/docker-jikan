be3dc67f671f26424726cccac883cb5dd85ce621
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-17 23:07:42 -0700

    optimize run/reset
    calling docker-compose up -d causes the images
    to build anyways, the run.sh can just be the
    one command that runs the image in the foreground

--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 docker-compose stop
 docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -aq)
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -aq)
--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-docker-compose stop
-docker-compose build
 docker-compose up
 # docker-compose up -d # daemonizes; I run this with screen/forever so it gets its own tty/output stream anyways
